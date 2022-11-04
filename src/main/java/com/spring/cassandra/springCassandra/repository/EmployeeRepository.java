bdb27188fabe1861674dabab9e61a5068b4dd5f2
1034517 <shashank.kumar@blueyonder.com>
2022-11-04 14:16:39 +0530

    cassandra db connected

---
+++ src/main/java/com/spring/cassandra/springCassandra/repository/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.spring.cassandra.springCassandra.repository;
+
+import com.spring.cassandra.springCassandra.model.Employee;
+import org.springframework.data.cassandra.repository.AllowFiltering;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface EmployeeRepository extends CassandraRepository<Employee, Integer> {
+
+    @AllowFiltering
+    List<Employee> findByName(String name);
+}
